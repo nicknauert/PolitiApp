98a050fe0546b84b5ede07230e7952f26bce4480
Nick Nauert <nicknauert@gmail.com>
2017-11-11 09:09:22 -0500

    init boilerplate

---
+++ frontend/src/containers/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className="dashboard">
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
